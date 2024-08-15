7f03d5f14aa3722f584b79edec6c62e4e56c54dd
Zer0sky <98915550+Zer0sky2804@users.noreply.github.com>
2024-08-15 16:21:58 +0200

    upravenaDatabaze

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Čtv 06. čen 2024, 18:15
+-- Vytvořeno: Čtv 15. srp 2024, 16:19
 -- Verze serveru: 10.4.32-MariaDB
 -- Verze PHP: 8.2.12
 
@@ -30,9 +30,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `nickname` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(455) NOT NULL,
   `email` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Vypisuji data pro tabulku `users`
+--
+
+INSERT INTO `users` (`user_id`, `nickname`, `password`, `email`) VALUES
+(4, 'admin', '$2y$10$bD7J/fbYCVE532moGG6VEOm0jpiqJsbaFfjzMfRHdeCaYTkcpKrTW', 'oskeram.04@spst.eu');
 
 --
 -- Indexy pro exportované tabulky
@@ -52,7 +59,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pro tabulku `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
